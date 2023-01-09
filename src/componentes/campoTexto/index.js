9a56c5f254accc05d62c93cc158fdf6ac6e4fcfa
Poul <poul.pheez2@gmail.com>
2023-01-09 13:59:17 -0300

    tentando armazenar infos

--- src/componentes/campoTexto/index.js
+++ src/componentes/campoTexto/index.js
@@ -1,12 +1,19 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    var valor = ''
+
+    const  aoDigitado = (evento) =>{
+        valor = evento.target.value
+        console.log(valor)
+    }
     return(
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 
